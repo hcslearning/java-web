a6ab8dac2c020608f1662f5035407bde418b9a3e
hcslearning <learning@hardcybersoft.cl>
2022-04-22 03:14:49 -0400

    maven example for Jakarte EE 9 Web Profile - Tomcat 10

---
+++ maven-web/src/main/java/controller/PruebaServlet.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PruebaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public PruebaServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
